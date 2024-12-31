93f4e2f53cadc975859f594b03b68e869eed8b14
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2024-12-31 23:33:40 +0530

    navbar nameicon blandemode setup done

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,35 +1,51 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+// import React, { useState } from "react";
 import Button from "../Button";
 
 function Navbar() {
+  // const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
-      <div className="container !max-w-full ">
-        <div className="flex justify-between h-20 items-center ">
-          <div className=" ">
-            <Link href="/">
-              <span className="text-xl font-bold uppercase">
-                Kuldeep&nbsp; Yadav
-              </span>
-            </Link>
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="size-11 border bg-stone-200 border-stone-400 rounded-full inline-flex items-center justify-center">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
-              </svg>
+    <header>
+      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md">
+        <div className="container !max-w-full ">
+          <div className="flex justify-between h-20 items-center ">
+            <div className=" ">
+              <Link href="/">
+                <span className="text-xl font-bold uppercase text-white">
+                  Kuldeep&nbsp; Yadav
+                </span>
+              </Link>
             </div>
-            <Button variant="primary" className=" hidden md:inline-flex ">
-              Contact Me
-            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="fixed top-0 left-0 w-full">
+        <div className="container !max-w-full ">
+          <div className="flex justify-end h-20 items-center ">
+            <div className="flex items-center gap-4">
+              <div className="size-11 border bg-stone-200 border-stone-400 rounded-full inline-flex items-center justify-center">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="3"
+                    y="15"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                  />
+                  <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+                </svg>
+              </div>
+              <Button variant="primary" className=" hidden md:inline-flex ">
+                Contact Me
+              </Button>
+            </div>
           </div>
         </div>
       </div>
