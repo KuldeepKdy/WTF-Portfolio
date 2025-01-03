5908cf50efc2b05f1e8d6028e862dbd0c912806a
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-01-04 00:27:40 +0530

    hero section text and buttons animation done

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,13 +9,19 @@ export default function Button(
   const { className, children, variant, iconAfter, ...rest } = props;
   return (
     <button
-      className={`" h-11 px-6 rounded-xl  border  uppercase inline-flex items-center gap-2 ",  
+      className={`"  rounded-xl  border  uppercase inline-flex items-center gap-2 transition-all duration-500 relative group/button ",  
         ${
           variant == "primary" &&
-          "bg-red-orange-500 border-red-orange-500 text-white"
+          "bg-red-orange-500 h-11 px-6 border-red-orange-500 text-white"
         }
-        ${variant == "secondary" && "border-red-orange-500"}
-        ${variant == "text" && "h-auto px-0 border-transparent"}
+        ${
+          variant == "secondary" &&
+          "border-red-orange-500 h-11 px-6 hover:bg-red-orange-500 hover:text-white "
+        }
+        ${
+          variant == "text" &&
+          "h-auto px-0 border-transparent after:transition-all after:duration-500  after:content-[''] after:h-px after:w-0 after:absolute after:top-full after:bg-red-orange-500 hover:after:w-full"
+        }
         ${className}`}
       {...rest}
     >
