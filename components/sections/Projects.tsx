87112d252634a2575935866acf12accd6db8c292
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-01-06 00:15:22 +0530

    projects section

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -39,9 +39,10 @@ const Projects = () => {
             <Link
               href="#"
               key={name}
-              className="border-t last:border-b border-stone-600 border-dotted py-6 md:py-8 lg:py-10 flex flex-col"
+              className="group border-t last:border-b border-stone-600 border-dotted py-6 md:py-8 lg:py-10 flex flex-col relative"
             >
-              <div className="">
+              <div className="absolute bottom-0 left-0 w-full h-0  group-hover:h-full transition-all duration-700 bg-stone-300"></div>
+              <div className="relative">
                 <div className="aspect-video md:hidden">
                   <Image
                     src={image}
@@ -49,22 +50,53 @@ const Projects = () => {
                     className="size-full object-cover"
                   />
                 </div>
-                <div className="mt-8 md:mt-0 flex justify-between items-center">
-                  <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
+                <div className="mt-8 md:mt-0 flex justify-between items-center md:grid md:grid-cols-[1fr_300px_max-content] md:gap-8">
+                  <div className="lg:group-hover:pl-8 transition-all duration-700">
+                    <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
+                  </div>
+                  <div className="relative">
+                    <div className="absolute aspect-video w-full  top-1/2 -translate-y-1/2 opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 lg:group-hover:scale-110 transition-all duration-500 z-10 ">
+                      <Image
+                        src={image}
+                        alt={name}
+                        className="size-full object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div className="lg:group-hover:pr-8 transition-all duration-700">
+                    <div className="size-6 overflow-hidden">
+                      <div className="h-6 w-12 flex group-hover:-translate-x-1/2 transition-transform duration-300">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                          />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </Link>
