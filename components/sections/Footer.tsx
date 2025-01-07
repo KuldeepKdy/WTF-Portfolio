2ae899005f65b7bfec566cf7f13efe81228f8ad7
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-01-07 16:46:58 +0530

    footer done

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,27 +1,38 @@
+"use client";
+import { motion } from "framer-motion";
 import Button from "../Button";
 
 const navItems = [
   {
-    href: "#",
-    label: "Home",
+    href: "#about",
+    label: "About",
   },
   {
-    href: "#",
+    href: "#projects",
     label: "Projects",
   },
   {
-    href: "#",
+    href: "#testimonials",
     label: "Testimonials",
   },
   {
-    href: "#",
+    href: "#faqs",
     label: "Faqs",
   },
   {
-    href: "#",
+    href: "#contact",
     label: "Contact",
   },
 ];
+const handleClickMobileNavItem = (e: React.MouseEvent<HTMLAnchorElement>) => {
+  e.preventDefault();
+  const url = new URL(e.currentTarget.href);
+  const hash = url.hash;
+  const target = document.querySelector(hash);
+
+  if (!target) return;
+  target.scrollIntoView({ behavior: "smooth" });
+};
 
 const Footer = () => {
   return (
@@ -29,31 +40,73 @@ const Footer = () => {
       <div className="container">
         <div className="section">
           <div className="flex items-center gap-3">
-            <div className="size-3 rounded-full bg-green-400"></div>
+            <div className="size-3 rounded-full bg-green-400 animate-pulse"></div>
             <span className="uppercase">One spot available for next month</span>
           </div>
           <div className="grid md:grid-cols-3 md:items-center">
-            <div className="md:col-span-2">
-              <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight">
-                Enough talk. Let&apos;s make something great together.
-              </h2>
+            <div className="md:col-span-2 ">
+              <div className="w-full flex flex-wrap">
+                {`Enough talk. Let's make something great together.`
+                  .split(" ")
+                  .map((data, index) => (
+                    <h2
+                      key={index}
+                      className="text-4xl flex md:text-7xl lg:text-8xl mt-8 font-extralight overflow-hidden"
+                    >
+                      <motion.span
+                        initial={{ translateY: "100%" }}
+                        whileInView={{
+                          translateY: "0%",
+                        }}
+                        className=""
+                        transition={{
+                          duration: 0.5,
+                          delay: index * 0.1,
+                          type: "spring",
+                          stiffness: 100,
+                        }}
+                      >
+                        {data}&nbsp;
+                      </motion.span>
+                    </h2>
+                  ))}
+              </div>
+
               <Button
                 variant="secondary"
                 iconAfter={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
+                  <div className="size-6 overflow-hidden">
+                    <div className="w-12 h-6 flex transition-transform duration-300 group-hover:-translate-x-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 }
                 className="mt-8"
               >
@@ -62,7 +115,7 @@ const Footer = () => {
             </div>
             <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
               {navItems.map(({ href, label }) => (
-                <a href={href} key={label}>
+                <a href={href} key={label} onClick={handleClickMobileNavItem}>
                   <Button variant="text" className="text-lg">
                     {label}
                   </Button>
